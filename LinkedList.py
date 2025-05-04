dd458df94938a206a0e50b7c245c960dcd56a1c7
Nibsquatch <rmauney@middlebury.edu>
2025-05-04 14:54:23 -0400

    LinkedList with add and remove

--- LinkedList.py
+++ LinkedList.py
@@ -10,6 +10,7 @@ class LinkedList:
     def __init__(self):
         self.head = None                # Pointer to the first node in the LinkedList
         self.tail = None                # Pointer to the last node in the LinkedList
+        self.numelements = 0            # Stores the number of elements in the list
 
     def addtoEnd(self, data):
         
@@ -24,24 +25,22 @@ class LinkedList:
             newNode.prev = self.tail    # the newNode's previous element is the old tail
             self.tail.next = newNode    # Link current tail's next to newNode
             self.tail = newNode         # Update the tail to newNode
+
+        self.numelements += 1
             
+    def deleteFront(self):
+        
+        if self.numelements == 0:       # return None if the list is empty
+            return None
+        
+        front = self.head.data          # Save the data to return
+        
+        if self.numelements == 1:       # If only one element was in the list
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next   # Move head to next node
+            self.head.prev = None        # Remove reference to old head
 
-
-    def delete_node(self, key):
-        current = self.head
-
-        if current and current.data == key:
-            self.head = current.next
-            current = None
-            return
-
-        prev = None
-        while current and current.data != key:
-            prev = current
-            current = current.next
-
-        if not current:
-            return  # Key not found
-
-        prev.next = current.next
-        current = None
+        self.numelements -= 1
+        return front
